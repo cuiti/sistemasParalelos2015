6091025dbc93d6b5abae385d4a4a8ff3b66fd833
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2015-07-02 13:10:33 -0300

    cambios de hoy, ya ejecuta sin violacion de segmnto

--- mpi.c
+++ mpi.c
@@ -12,30 +12,32 @@ double dwalltime(){		//Para calcular tiempo
 
 
 int main(int argc,char*argv[]){
-	int a[10],b[10],identificador, cantidadProcesos, N, activeWorkers; 			//identificador es el numero de proceso
+	int a[10],b[10],identificador, cantidadProcesos, N, activeWorkers,flag,workerNumber, size; 			//identificador es el numero de proceso
 	MPI_Init(&argc, &argv );
 	MPI_Status *estado;
 	MPI_Comm_size(MPI_COMM_WORLD, &cantidadProcesos ); 
 	MPI_Comm_rank(MPI_COMM_WORLD, &identificador ); 
-	int i,j,k,finished=0;
-	double timetick, *arreglo, aux, dimension, *paquete, size;
+	int i,j,k,l,finish=0;
+	double timetick, *arreglo, aux, dimension, *paquete,*merge;
 	
 	
 	
 		
 	if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ){
-		printf("\nUsar: %s n\n  n: Tamaño de la porcion que reciben los slaves\n", argv[0]);	//En N queda el tamaño de las partes
+		printf("\nUsar: %s n\n  n: TamaÃ±o de la porcion que reciben los slaves\n", argv[0]);	//En N queda el tamaÃ±o de las partes
 		exit(1);
 	}
 	
-	dimension = 1048576;	//la dimension del arreglo a ordenar es 2^20
+	dimension=1024; //dimension = 1048576;	//la dimension del arreglo a ordenar es 2^20
 	int cantBloques = dimension/N;
 	
 	activeWorkers=0;
 
+	merge=(double*)malloc(sizeof(double));
+
 //TAG "1" hace sort
 //tag "2" merge
-//TAG "3" significa que se completó el programa
+//TAG "3" significa que se completÃ³ el programa
 //tag "4" para devolver arreglos
 
 
@@ -47,7 +49,7 @@ int main(int argc,char*argv[]){
 		
 		
 		srand((unsigned int)time(NULL));
-		for(i=0;i<dimension-1;i++){		//Inicializa el arreglo con numeros random
+		for(i=0;i<dimension;i++){		//Inicializa el arreglo con numeros random
 			//arreglo[i]=rand();
 		  arreglo[i]=1;
 		}
@@ -56,28 +58,28 @@ int main(int argc,char*argv[]){
 		
 		paquete=(double*)malloc(sizeof(double)*N+1);
 		
-		
+		timetick = dwalltime();
 		for(i=0; i<cantBloques; i++){				//Copia las porciones y las envia a los workers
 		
 			int k=0;
 			for (j=i*N; j<(i+1)*N; j++){
-				paquete[k]= j;
+				paquete[k]= arreglo[j];
 				k++;
 			}
-			paquete[N+1]=i*N;	//la posicion de la porcion queda en el ultimo lugar del arreglo
+			paquete[N]=i*N;	//la posicion de la porcion queda en el ultimo lugar del arreglo
 
-			int workerNumber;
+			
 			MPI_Recv(workerNumber, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, estado);   
 		  		
 			MPI_Send(paquete,1,MPI_DOUBLE,workerNumber,1,MPI_COMM_WORLD); 				//Chequea si hay workers que ya terminaron					 
 			activeWorkers++;
   
-			if (MPI_Iprobe(MPI_ANY_SOURCE,4, MPI_COMM_WORLD, estado)){						
+			if (MPI_Iprobe(MPI_ANY_SOURCE,4, MPI_COMM_WORLD, flag, estado)){						
 				MPI_Recv(paquete, N+1, MPI_DOUBLE, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, estado);
 				activeWorkers--;
 				for(j=0; j<N; j++){
-					arreglo[paquete[N+1]]=paquete[j];
-					paquete[N+1]++;
+					arreglo[(int)paquete[N]]=paquete[j];
+					paquete[N]++;
 				}
 
 			}
@@ -86,44 +88,42 @@ int main(int argc,char*argv[]){
 			MPI_Recv(paquete, N+1, MPI_DOUBLE, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, estado);
 			activeWorkers--;
 			for(j=0; j<N; j++){
-				arreglo[paquete[N+1]]=paquete[j];
-				paquete[N+1]++;
+				arreglo[(int)paquete[N]]=paquete[j];
+				paquete[N]++;
 			}
 		
 		}
 
 		N=N*2;
-		activeWorkers=0;
 		while (N<=dimension){
 			cantBloques=cantBloques/2;
 			for(i=0; i<cantBloques; i++){
 				int k=0;
 				for (j=i*N; j<(i+1)*N; j++){
-				paquete[k]= j;
-		    	k++;
-		 		 }	
+					paquete[k]= j;
+		    		k++;
+		 		}	
 			}
-			paquete[N+1]=i*N;	//el inicio de la porcion queda en la ultima posicion del arreglo
+			paquete[N]=i*N;	//el inicio de la porcion queda en la ultima posicion del arreglo
 			
 			
 			MPI_Recv(workerNumber, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, estado); 
-			MPI_Send(paquete,1,MPI_DOUBLE,workerNumber,2,MPI_COMM_WORLD);	//Envía primero el paquete y después el tamaño de bloque N
-			MPI_Send(N,1,MPI_DOUBLE,workerNumber,2,MPI_COMM_WORLD);
-			activeWorkers++;
+			MPI_Send(N,1,MPI_INT,workerNumber,2,MPI_COMM_WORLD);
+			MPI_Send(paquete,1,MPI_DOUBLE,workerNumber,2,MPI_COMM_WORLD);	//EnvÃ­a primero el tamaÃ±o de bloque y despuÃ©s el paquete		
 
-			if (MPI_Probe(MPI_ANY_SOURCE,4, MPI_COMM_WORLD, estado)){						
+			if (MPI_Iprobe(MPI_ANY_SOURCE,4, MPI_COMM_WORLD,flag, estado)){						
 				MPI_Recv(paquete, N+1, MPI_DOUBLE, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, estado);
-				activeWorkers--;
 				for(j=0; j<N; j++){
-					arreglo[paquete[N+1]]=paquete[j];
-					paquete[N+1]++;
+					arreglo[(int)paquete[N]]=paquete[j];
+					paquete[N]++;
 				}
 
 			}
 			
 			N=N*2;
 		}
-		
+		printf("Tiempo en segundos %f\n", dwalltime() - timetick);
+
 
 		MPI_Send(N,1,MPI_DOUBLE,workerNumber,3,MPI_COMM_WORLD); //send para halt
 			
@@ -145,7 +145,7 @@ int main(int argc,char*argv[]){
 
 					//aca ordena
 					for(i=1;i<N;i++) {
-			     			for(j=0;j<N-i;j++) {
+			     		for(j=0;j<N-i;j++) {
 				  			if(paquete[j]>=paquete[j+1]) {
 				   				aux=paquete[j];
 				   				paquete[j]=paquete[j+1];
@@ -153,43 +153,44 @@ int main(int argc,char*argv[]){
 				  			}
 				 		}
 					}
-					MPI_send(paquete,N+1,MPI_DOUBLE,0,4,MPI_COMM_WORLD);
+					MPI_Send(paquete,N+1,MPI_DOUBLE,0,4,MPI_COMM_WORLD);
 			
 				}
 				else if(MPI_Probe(0,2, MPI_COMM_WORLD, estado)){						//Hay mensaje con flag 2, entonces pasa al merge
-					MPI_Recv(paquete, N+1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, estado);
-					MPI_Recv(size, 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, estado);
+					MPI_Recv(size, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, estado);
+					paquete=(double*)realloc(paquete,sizeof(double)*size+1);
+					MPI_Recv(paquete, size+1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, estado);
 					merge=(double*)realloc(merge,sizeof(double)*size);
-			    		j=0;
-			    		k=size/2;
-			    		l=0;
-			    		while(j<size/2 && k<size) {
-			     			if(paquete[j]<=paquete[k]){
-			      				merge[l]=paquete[j];
-			      				j++;
-			     			}else{
-			      				merge[l]=paquete[k];
-			      				k++;
-			     			}
-			     			l++;
-			    		}
-			    		while(j<size/2) {
-			     			merge[l]=paquete[j];
-			     			j++;
-			     			l++;
-			    		}
-			    		while(k<size) {
-			     			merge[l]=paquete[k];
-			     			k++;
-			     			l++;
-			    		}
-			    		for(j=0;j<size;j++){
-			     			paquete[j]=merge[j];
-			    		}
-			   	}
-				MPI_send(paquete,size,MPI_DOUBLE,0,4,MPI_COMM_WORLD);
+		    		j=0;
+		    		k=size/2;
+		    		l=0;
+		    		while(j<size/2 && k<size) {
+		     			if(paquete[j]<=paquete[k]){
+		      				merge[l]=paquete[j];
+		      				j++;
+		     			}else{
+		      				merge[l]=paquete[k];
+		      				k++;
+		     			}
+		     			l++;
+		    		}
+		    		while(j<size/2) {
+		     			merge[l]=paquete[j];
+		     			j++;
+		     			l++;
+		    		}
+		    		while(k<size) {
+		     			merge[l]=paquete[k];
+		     			k++;
+		     			l++;
+		    		}
+		    		for(j=0;j<size;j++){
+		     			paquete[j]=merge[j];
+		    		}
+		   	}
+				MPI_Send(paquete,size,MPI_DOUBLE,0,4,MPI_COMM_WORLD);
 			}
-		}
-			
-	MPI_Finalize();
+		}			
+	}
+	MPI_Finalize();	
 }
